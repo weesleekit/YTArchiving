5186fdf082f9b7601a84a98c1ca48a9746fa93f3
weesleekit <weesleekit@sleekit.co.uk>
2023-04-06 15:06:10 +0100

    Add project files.

---
+++ YTArchiving/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace YTArchiving
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<string> channels = new()
+            {
+                "3blue1brown", // Education - Maths
+                "brandonylee", // Education - Cameras & Lighting
+            };
+
+            foreach (string channel in channels)
+            {
+                await PlayListDownloader.Download(channel);
+            }
+        }
+    }
+}
